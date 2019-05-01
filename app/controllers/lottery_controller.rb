9adc5b502b3dbd6ac782ff3d5f7a1a1afaf574d3
TheAkbar <shoaib429@gmail.com>
2019-04-30 23:39:57 -0400

    Rather than creating records in the bids table to merge team users bids, return a Hash that maps team_ids to a list of their preferred topics in descending order

--- app/controllers/lottery_controller.rb
+++ app/controllers/lottery_controller.rb
@@ -34,8 +34,8 @@ class LotteryController < ApplicationController
       response = RestClient.post url, bidding_data.to_json, content_type: :json, accept: :json
       teams = JSON.parse(response)["teams"]
       ExpertizaLogger.info LoggerMessage.new(controller_name, session[:user].name, "Team formation info for assignment #{assignment.name}: #{teams}", request)
-      create_new_teams_for_bidding_response(teams, assignment, priority_info)
-      match_new_teams_to_topics(assignment)
+      team_to_topic_info = create_new_teams_for_bidding_response(teams, assignment, priority_info)
+      match_new_teams_to_topics(assignment, team_to_topic_info)
     rescue StandardError => e
       flash[:error] = e.message
     end
@@ -45,6 +45,16 @@ class LotteryController < ApplicationController
   private
 
   def create_new_teams_for_bidding_response(teams, assignment, priority_info)
+    # Holds a map between team id and list of topics the team prefers
+    # For example below, team id 3  prefers topic 2 and then topic 3
+    # {
+    #   1: [1, 4, 3, 5],
+    #   2: [0, 1, 2],
+    #   3: [2, 3],
+    #   4: [2, 0, 1]
+    # }
+    # This map is returned at the end of this method
+    topic_priority_dict = Hash.new
     # Structure of teams variable: [[user_id1, user_id2], [user_id3, user_id4]]
     teams.each do |user_ids|
       # Create new team and team node
@@ -61,13 +71,14 @@ class LotteryController < ApplicationController
         # Create new team_user and team_user node
         new_team_user = TeamsUser.create(user_id: user_id, team_id: new_team.id)
         TeamUserNode.create(parent_id: team_node.id, node_object_id: new_team_user.id)
-        # Create new bids for team based on `bidding_data` variable for each team member
-        # Currently, it is possible (already proved by db records) that
-        # some team has multiple 1st priority, multiply 2nd priority, ....
-        # these multiple identical priorities come from different previous teams
-        # [Future work]: we need to find a better way to merge bids that came from different previous teams
-        merge_bids_from_different_previous_teams(assignment, new_team.id, user_ids, priority_info)
       end
+      # Create new bids for team based on `bidding_data` variable for each team member
+      # Currently, it is possible (already proved by db records) that
+      # some team has multiple 1st priority, multiply 2nd priority, ....
+      # these multiple identical priorities come from different previous teams
+      # [Future work]: we need to find a better way to merge bids that came from different previous teams
+      topic_priority_info = merge_bids_from_different_previous_teams(assignment, new_team.id, user_ids, priority_info)
+      topic_priority_dict[new_team.id] = topic_priority_info
     end
     # Remove empty teams
     assignment.teams.reload.each do |team|
@@ -76,6 +87,7 @@ class LotteryController < ApplicationController
         team.destroy
       end
     end
+    return topic_priority_dict
   end
 
   def merge_bids_from_different_previous_teams(assignment, team_id, user_ids, priority_info)
@@ -120,14 +132,15 @@ class LotteryController < ApplicationController
     #   [2, 3, 4]
     # ]
     # Therefore the bidding priority of these 4 topics is 1 -> 3 -> 2 -> 4
-    bidding_matrix_summary.each_with_index do |b, index|
-      Bid.create(topic_id: b[2], team_id: team_id, priority: index + 1)
-    end
+    # Returns the list of topics that the team bid for in order of priority
+    return bidding_matrix_summary.collect{|b| b[2]}
   end
 
   # This method is called for assignments which have their is_intelligent property set to 1.
   # It runs a stable match algorithm and assigns topics to strongest contenders (team strength, priority of bids)
-  def match_new_teams_to_topics(assignment)
+  def match_new_teams_to_topics(assignment, team_to_topic_info)
+    # Takes in an assignment and a Hash that maps teams to an array that lists the teams topic
+    # preferences in order and then attempts to match all teams to their preferred topics
     unless assignment.is_intelligent
       flash[:error] = "This action is not allowed. The assignment #{assignment.name} does not enable intelligent assignments."
       return
@@ -147,6 +160,10 @@ class LotteryController < ApplicationController
     # Generate team bidding infomation hash based on newly-created teams
     team_bids = []
     unassigned_teams.each do |team|
+      if team_to_topic_info.key?(team.id)
+        team_bids << team_to_topic_info[team.id]
+        next
+      end
       topic_bids = []
       sign_up_topics.each do |topic|
         bid = Bid.find_by(team_id: team.id, topic_id: topic.id)
